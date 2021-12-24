5def8682840cf336c949ee8342464b68f7c7286a
M Agung Sutrisno <m.agung.sutrisno@gmail.com>
2021-12-24 22:14:28 +0700

    Add course gateway

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -7,6 +7,7 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const mediaRouter = require('./routes/media');
+const coursesRouter = require('./routes/courses');
 
 
 const app = express();
@@ -20,5 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/media', mediaRouter);
+app.use('/courses', coursesRouter);
 
 module.exports = app;
