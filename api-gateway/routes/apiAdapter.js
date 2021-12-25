24b4290bbfb7dd4b2e59371cd4b5f08208b75a30
M Agung Sutrisno <m.agung.sutrisno@gmail.com>
2021-12-25 11:31:11 +0700

    API Gateway - Fix typo apiAdapter

--- api-gateway/routes/apiAdapter.js
+++ api-gateway/routes/apiAdapter.js
@@ -4,7 +4,7 @@ const { HIT_TIMEOUT } = process.env;
 
 module.exports = (baseUrl) => {
     return axios.create({
-        baseUrl:baseUrl,
-        timeout: HIT_TIMEOUT
+        baseURL:baseUrl,
+        timeout: parseInt(HIT_TIMEOUT)
     });
 }
